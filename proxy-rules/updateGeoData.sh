6e0952db61cf6029bbd973c2e9963e8a03c6185a
Crane Leeon <craneleeon@gmail.com>
2024-08-15 11:46:22 +0800

    update

--- proxy-rules/updateGeoData.sh
+++ proxy-rules/updateGeoData.sh
@@ -1,3 +1,3 @@
 curl -L -O https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
 curl -L -O https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat
-curl -L -O https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
+curl -L -o Country.mmdb https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
